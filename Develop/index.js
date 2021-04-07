7ca4b7d971ded730abdfeacc9e9a214e62490221
Chris Ruiz <chrispruiz@att.net>
2021-04-06 22:37:35 -0500

    Added first question and added package declarations

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,18 @@
 // TODO: Include packages needed for this application
+ const inquirer = require('inquirer');
+ const fs = require('fs');
  
 // TODO: Create an array of questions for user input
 const questions = [];
-
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the title of your project?'
+        }
+    ])
+    .then(answers => console.log(answers));
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
