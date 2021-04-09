853c57e67763477a1a40425a27b0a029beda46b0
Chris Ruiz <chrispruiz@att.net>
2021-04-08 23:11:49 -0500

    questions now generate to readme and changed formatting

--- Develop/index.js
+++ Develop/index.js
@@ -1,20 +1,97 @@
 // TODO: Include packages needed for this application
  const inquirer = require('inquirer');
  const fs = require('fs');
+ const generateMarkdown = require('./utils/generateMarkdown');
  
-// TODO: Create an array of questions for user input
-const questions = [];
-inquirer
-    .prompt([
-        {
+ 
+ 
+// Create an array of questions for user input
+
+        const question1 = {
             type: 'input',
-            name: 'Title',
-            message: 'What is the title of your project?'
-        }
-    ])
-    .then(answers => console.log(answers));
+            name: 'github',
+            message: 'Enter your Github Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github Username!');
+                    return false;
+                }
+            }
+        };
+        const question2 = {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false;
+                }
+            }
+        };
+        const question3 = {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        };
+        const question4 = {
+            type: 'input',
+            name: 'description',
+            message: 'Write a short description of your project:'
+          };
+          const question5 = {
+            type: 'input',
+            name: 'installationInstruction',
+            message: 'Provide installation instructions of your project:'
+          };
+          const question6 = {
+            type: 'input',
+            name: 'usageInformation',
+            message: 'Enter usage information:'
+          };
+          const question7 = {
+            type: 'input',
+            name: 'contributionGuidelines',
+            message: 'Enter contributions:'
+          };
+          const question8 = {
+            type: 'input',
+            name: 'testInstructions',
+            message: 'Enter instructions for tests:'
+          };
+          const question9 = {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your application:',
+            choices: ['MIT_License', 'GNU_GPLv3', 'Apache_License_2.0', 'None']
+          };   
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+.prompt([question1, question2, question3, question4, question5, question6, question7, question8, question9])
+.then(function(data) {
+    var filename = data.title + '.md'; 
+
+    console.log(data);
+    fs.writeFile(filename, generateMarkdown(data), function(err) {
+        if (err) throw err;
+      
+        console.log('README complete! Check out README.me to see the output!');
+      });
+})
 
 // TODO: Create a function to initialize app
 function init() {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `![image](https://img.shields.io/badge/license-${license}-blue.svg)`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,7 +16,19 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## Description: 
+  ${data.description}
+  ## Table of Contents
+    * [Installation](#installation) 
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+  ## Installation
+  Run the following code in the command line to run dependencies:
+  ${data.installationInstruction}
+  ## License:
+  
 `;
 }
 
